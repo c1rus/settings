77686d67cb626e588ea286565de439296e6d6609
pluptak <petolup@gmail.com>
2018-04-03 10:10:49 +0200

    initial

---
+++ vscode.js
@@ -0,0 +1,69 @@
+// Place your settings in this file to overwrite the default settings
+{
+    "editor.fontFamily": "Inconsolata, Menlo, Monaco, 'Courier New', monospace",
+    "editor.fontSize": 14,
+    "editor.lineHeight": 25,
+    "editor.letterSpacing": 0.5,
+    "files.trimTrailingWhitespace": true,
+    "editor.fontWeight": "400",
+    "editor.cursorStyle": "line",
+    "editor.cursorWidth": 5,
+    "editor.cursorBlinking": "solid",
+    // Very important: Install this plugin: https://github.com/be5invis/vscode-custom-css
+    // you'll need to change this to an absolute path on your computer
+    "vscode_custom_css.imports": [
+        "/Volumes/Macintosh HD/Users/pluptak/.vscodestyles.css"
+    ],
+    "editor.renderWhitespace": "all",
+    "workbench.editor.enablePreviewFromQuickOpen": false,
+    "editor.formatOnSave": true,
+    "tslint.rulesDirectory": "node_modules/codelyzer",
+    "files.exclude": {
+        "**/.git": true,
+        "**/.svn": true,
+        "**/.hg": true,
+        "**/.DS_Store": true,
+        "**/node_modules": true,
+        "**/bin": true,
+        "**/obj": true
+    },
+    "gitProjectManager.baseProjectsFolders": [
+        "c:\\projects"
+    ],
+    "autoimport.doubleQuotes": true,
+    "mssql.connections": [
+        {
+            "authenticationType": "SqlLogin",
+            "server": "0.0.0.0:1433",
+            "database": "",
+            "user": "sa",
+            "password": "",
+            "savePassword": true,
+            "profileName": "docker"
+        },
+        {
+            "authenticationType": "SqlLogin",
+            "server": "localhost",
+            "database": "",
+            "user": "sa",
+            "password": "",
+            "savePassword": true,
+            "profileName": "mssql"
+        }
+    ],
+    "workbench.iconTheme": "vs-seti",
+    "sync.gist": "233b2c66dd812c337f36091cc876f958",
+    "sync.lastUpload": "2017-05-12T13:06:39.676Z",
+    "sync.autoDownload": false,
+    "sync.autoUpload": false,
+    "sync.lastDownload": "",
+    "sync.version": 262,
+    "sync.showSummary": true,
+    "sync.forceDownload": false,
+    "sync.anonymousGist": false,
+    "sync.host": "",
+    "sync.pathPrefix": "",
+    "extensions.autoUpdate": true,
+    "git.autofetch": true,
+    "workbench.colorTheme": "Cobalt2"
+}
